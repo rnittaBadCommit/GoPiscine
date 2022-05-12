e90233885dcbf7c3fc02529f79225c91ca6d913e
rnitta <ryushironitta@yahoo.co.jp>
2022-05-12 22:28:37 +0900

    OK

--- 05/ex06/main.go
+++ 05/ex06/main.go
@@ -1,10 +1,24 @@
 package main
-
 import (
-	"piscine"
+"fmt"
+"piscine"
 )
-
 func main() {
-	a := piscine.SplitWhiteSpaces("Hello how are you?")
-	piscine.PrintWordsTables(a)
+	s := "HelloHAhowHAareHAyou?"
+	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
+	s = "HAHelloHAhowHAareHAyou?"
+	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
+	s = "HelloHAhowHAareHAyou?HA"
+	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
+	s = "HelloHAhowHAareHAyouHA?"
+	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
+	s = "HAHAHelloHAhowHAareHAHAyou?HAHA"
+	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
+	s = ""
+	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
+	s = "HAHAHelloHAhowHAareHAHAyou?HAHA"
+	fmt.Printf("%#v\n", piscine.Split(s, ""))
+	s = ""
+	fmt.Printf("%#v\n", piscine.Split(s, ""))
 }
+
