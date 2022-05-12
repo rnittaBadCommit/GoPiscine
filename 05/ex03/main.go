f2acab4d4c4bfdd324034f33e11e134a3ff5fce3
rnitta <ryushironitta@yahoo.co.jp>
2022-05-12 22:29:52 +0900

    add case

--- 05/ex03/main.go
+++ 05/ex03/main.go
@@ -11,5 +11,6 @@ func main() {
 	fmt.Printf("%#v\n", piscine.SplitWhiteSpaces("  Hello how are you ?"))
 	fmt.Printf("%#v\n", piscine.SplitWhiteSpaces("Hellohowareyou?"))
 	fmt.Printf("%#v\n", piscine.SplitWhiteSpaces(" Hellohowareyou? "))
+	fmt.Printf("%#v\n", piscine.SplitWhiteSpaces("    "))
 	fmt.Printf("%#v\n", piscine.SplitWhiteSpaces(""))
 }
