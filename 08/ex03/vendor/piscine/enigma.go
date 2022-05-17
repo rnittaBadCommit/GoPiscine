45780e43c391408839e991d8d49e01e6be3f5c7f
Ryushiro Nitta <rnitta@c1r13s2.42tokyo.jp>
2022-05-17 18:12:00 +0900

    OK

---
+++ 08/ex03/vendor/piscine/enigma.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	if a != nil && *a != nil && **a != nil {
+		if b != nil {
+			if c != nil && *c != nil && **c != nil && ***c != nil && ****c != nil && *****c != nil && ******c != nil {
+				if d != nil && *d != nil && **d != nil && ***d != nil {
+					***a, *b, *******c, ****d = *b, ****d, ***a, *******c
+				}
+			}
+		}
+	}
+}
