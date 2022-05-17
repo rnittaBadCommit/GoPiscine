d15c078a84ccea6623867c9843c5557ac1ac5e05
Ryushiro Nitta <rnitta@c1r13s2.42tokyo.jp>
2022-05-17 16:21:18 +0900

    OK

---
+++ 08/ex01/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	median := piscine.Median(2, 3, 8, 5, 7)
+
+	fmt.Println(median)
+}
---
+++ 08/ex01/vendor/piscine/median.go
@@ -0,0 +1,6 @@
+package piscine
+
+func Median(a, b, c, d, e int) int {
+	sum := a + b + c + d + e
+	return sum / 5
+}
